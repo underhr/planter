8bf8f2872026bd5e72cb166beb40e6d11c18fead
underhr <haleyroseu@gmail.com>
2025-11-17 01:30:42 -0600

    updated image links

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 
 function Stool({ bottom, left, right }) {
   return(
-    <img src="./images/stool.png" alt="stool" className={`absolute h-30`} style={{bottom, left, right}}/>
+    <img src="/planter/images/stool.png" alt="stool" className={`absolute h-30`} style={{bottom, left, right}}/>
   );
 }
 
 function Plant({ stage, bottom, left, right }) {
   return(
-    <img src={`./images/plants/${stage}.png`} alt={`${stage} plant`} className={`absolute h-30`} style={{bottom, left, right}} />
+    <img src={`/planter/images/plants/${stage}.png`} alt={`${stage} plant`} className={`absolute h-30`} style={{bottom, left, right}} />
   );
 }
 
@@ -15,9 +15,9 @@ function App() {
 
   return (
     <div className="flex min-h-screen w-screen items-center justify-center">
-      <div className="max-h-[360px] w-full max-w-[500px] aspect-3/5 bg-[#c0aa8a] rounded-2xl p-4 bg-[url('./images/background.png')] bg-cover relative">
+      <div className="max-h-[360px] w-full max-w-[500px] aspect-3/5 bg-[#c0aa8a] rounded-2xl p-4 bg-[url('/planter/images/background.png')] bg-cover relative">
 
-        <img src="./images/window.png" alt="window" className="absolute top-0 left-25 h-40"/>
+        <img src="/planter/images/window.png" alt="window" className="absolute top-0 left-25 h-40"/>
 
         <Stool bottom={16} left={24}/>
         <Plant bottom={112} left={24} stage="teen" />
